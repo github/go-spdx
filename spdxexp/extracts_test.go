16353ede453549eb59f7798243b003c6937ae16a
Andrew Henry <ajhenry@github.com>
2023-09-19 09:41:26 -0400

    test: update to include error test

--- spdxexp/extracts_test.go
+++ spdxexp/extracts_test.go
@@ -17,13 +17,18 @@ func TestExtractLicenses(t *testing.T) {
 		{"AND'ed & OR'ed licenses", "(MIT AND Apache-2.0) OR GPL-3.0", []string{"GPL-3.0", "MIT", "Apache-2.0"}},
 		{"ONLY modifiers", "LGPL-2.1-only OR MIT OR BSD-3-Clause", []string{"MIT", "BSD-3-Clause", "LGPL-2.1-only"}},
 		{"WITH modifiers", "GPL-2.0-or-later WITH Bison-exception-2.2", []string{"GPL-2.0-or-later+ WITH Bison-exception-2.2"}},
+		{"Invalid SPDX expression", "MIT OR INVALID", nil},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			licenses, err := ExtractLicenses(test.inputExpression)
 			assert.ElementsMatch(t, test.extractedLicenses, licenses)
-			assert.NoError(t, err)
+			if test.extractedLicenses == nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }

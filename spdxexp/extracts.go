7fe1a57f224bfb7bad17dc0783a9e94717048f24
Andrew Henry <ajhenry@github.com>
2023-09-19 09:42:04 -0400

    fix: remove nil guard

--- spdxexp/extracts.go
+++ spdxexp/extracts.go
@@ -1,7 +1,5 @@
 package spdxexp
 
-import "errors"
-
 // ExtractLicenses extracts licenses from the given expression without duplicates.
 // Returns an array of licenses or error if error occurs during processing.
 func ExtractLicenses(expression string) ([]string, error) {
@@ -14,10 +12,6 @@ func ExtractLicenses(expression string) ([]string, error) {
 	licenses := make([]string, 0)
 	allLicenses := flatten(expanded)
 	for _, licenseNode := range allLicenses {
-		if licenseNode == nil {
-			return nil, errors.New("license node is nil")
-		}
-
 		licenses = append(licenses, *licenseNode.reconstructedLicenseString())
 	}
 
